c8493eb9215b29048615b3b9034ac2a626168e6c
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-07-01 12:53:35 -0500

    fis: issue with the jwt in the test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
 from main import app
+import os
 
 @pytest.fixture
 def client():
@@ -10,22 +11,24 @@ def client():
 
 @patch("services.functions.conection_accounts")
 def test_login_success(mock_conection, client):
-    # Simmulate a database connection
+    # Simulate the secret key for JWT
+    os.environ["SECRET_KEY"] = "test-secret-key"
+
+    # Simulate the database connection
     mock_session = MagicMock()
     mock_conection.return_value = mock_session
 
     # Simulate a user in the database
     fake_user = MagicMock()
     fake_user.Status = 1
-    fake_user.Password = "hashed123"
     fake_user.Id_User = 1
     fake_user.User_mail = "juan@example.com"
-    mock_session.query().filter().one.return_value = fake_user
 
-    # Simmulate the password hashing function
     from services.functions import hash_password
     plain_password = "mipassword"
     fake_user.Password = hash_password(plain_password)
+
+    mock_session.query().filter().one.return_value = fake_user
 
     response = client.post("/login", json={
         "User_mail": "juan@example.com",
